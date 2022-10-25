8bc102b0fb58c8b66572d82916922965ec7b6cfb
Rares Popa <43013784+Rares1511@users.noreply.github.com>
2022-10-25 21:57:52 +0300

    Add files via upload

---
+++ src/CalcFrame.java
@@ -0,0 +1,151 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import javax.swing.JButton;
+
+class MyButton {
+
+    JButton button;
+    int mark;
+    static int numberOfButtons = 0;
+
+    MyButton ( JButton button, int mark ) {
+        this.button = button;
+        this.mark = mark;
+        numberOfButtons++;
+    }
+
+}
+
+public class CalcFrame extends JFrame implements ActionListener, ComponentListener {
+
+    public static JFrame frame;
+    public final static Rectangle frameSize = new Rectangle ( 0, 0, 350, 650 );
+    public static JTextField textField;
+    static final int numberOfLines = 6;
+    static final int numberofColums = 4;
+    static MyButton[] myButtons = new MyButton[30];
+
+    CalcFrame ( ) { }
+
+    private MyButton getButton ( String id, CalcFrame calcFrame ) {
+
+        JButton button = new JButton ( id );
+        button.addActionListener ( calcFrame );
+        button.setVerticalAlignment ( JButton.CENTER );
+        return new MyButton ( button, MyButton.numberOfButtons );
+
+    }
+
+    private MyButton getButton ( Icon icon, CalcFrame calcFrame ) {
+        JButton button = new JButton ( icon );
+        button.addActionListener ( calcFrame );
+        button.setVerticalAlignment ( JButton.CENTER );
+        return new MyButton ( button, MyButton.numberOfButtons );
+    }
+
+    @Deprecated
+    public static void main ( String[] args ) {
+
+        CalcFrame calcFrame = new CalcFrame ( );
+
+        frame = new JFrame ( "Calculator" );
+        frame.setBounds ( frameSize );
+        frame.setMinimumSize ( frame.getSize ( ) );
+
+        textField = new JTextField ( );
+
+        Icon icon = new ImageIcon ( "x squared.jpg" );
+        String[] buttonSigns = { "%", "CE", "C", "←", "¹/ₓ", "x²", "√", "÷", "7", "8", "9", "×", "6", "5", "4", "-",
+                "1", "2", "3", "+", "±", "0", ",", "=" };
+
+        for ( int i = 0; i < buttonSigns.length; i++ ) {
+            myButtons[i] = calcFrame.getButton ( buttonSigns[i], calcFrame );
+        }
+
+        calcFrame.componentResized ( null );
+
+        try { UIManager.setLookAndFeel ( UIManager.getSystemLookAndFeelClassName ( ) ); }
+        catch ( Exception e ) { e.printStackTrace ( ); }
+
+        textField.setEditable ( false );
+        textField.setBackground ( Color.LIGHT_GRAY );
+
+        for ( int i = 0; i < MyButton.numberOfButtons; i++ )
+            frame.add ( myButtons[i].button );
+
+        frame.add ( textField );
+        frame.setLayout ( null );
+        frame.setVisible ( true );
+        frame.show ( );
+
+        frame.addComponentListener ( calcFrame );
+        frame.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
+
+    }
+
+    @Override
+    public void actionPerformed ( ActionEvent e ) {
+        textField.setText ( textField.getText ( ) + e.getActionCommand ( ) );
+    }
+
+    @Override
+    public void componentResized ( ComponentEvent e ) {
+        System.out.println ( "It has been resized" );
+        Dimension actualSize = frame.getContentPane().getSize();
+
+        int frameWidth = ( int ) actualSize.getWidth ( );
+        int frameHeight = ( int ) actualSize.getHeight ( );
+
+        int spaceBetweenX = frameWidth / 100;
+        int spaceBetweenY = frameHeight / 100;
+        int finalX = frameWidth - 2 * spaceBetweenX;
+        int finalY = frameHeight / 5;
+
+        int fontSize = ( int ) Point.distance ( 0, 0, frameHeight, frameWidth ) / 21;
+        Font buttonFont = new Font ( "Calibri", Font.PLAIN, fontSize );
+
+        Rectangle textFieldSize = new Rectangle ( spaceBetweenX, spaceBetweenY, finalX, finalY );
+        Rectangle[] bounds = new Rectangle[30];
+
+        int buttonHeight = ( frameHeight - textFieldSize.height - spaceBetweenY * ( numberOfLines + 2 ) ) / numberOfLines;
+        int buttonWidth = ( frameWidth - spaceBetweenX * ( numberofColums + 3 ) ) / numberofColums;
+
+        for ( int i = 0; i < numberOfLines; i++ ) {
+            for ( int j = 0; j < numberofColums; j++ ) {
+                int position = i * numberofColums + j;
+                int startX = j * buttonWidth + ( j + 1 ) * spaceBetweenX;
+                int startY = i * buttonHeight + ( int ) textFieldSize.getMaxY ( ) + ( i + 1 ) * spaceBetweenY;
+                int width = buttonWidth;
+                if ( j == numberofColums - 1 )
+                    width = actualSize.width - ( int ) textFieldSize.getMinX ( ) - startX;
+                bounds[position] = new Rectangle ( startX, startY, width, buttonHeight );
+            }
+        }
+
+        for ( int i = 0; i < MyButton.numberOfButtons; i++ ) {
+            myButtons[i].button.setBounds ( bounds[myButtons[i].mark] );
+            myButtons[i].button.setFont ( buttonFont );
+        }
+        textField.setBounds ( textFieldSize );
+        textField.setFont ( buttonFont );
+    }
+
+    @Override
+    public void componentMoved(ComponentEvent e) {
+        System.out.println ( "It has been moved" );
+    }
+
+    @Override
+    public void componentShown(ComponentEvent e) {
+        System.out.println ( "It has been shown" );
+    }
+
+    @Override
+    public void componentHidden(ComponentEvent e) {
+        System.out.println ( "It has been hidden" );
+    }
+}
---
+++ src/Calculator.java
@@ -0,0 +1,38 @@
+public class Calculator {
+
+    final static String DIGIT0 = "0";
+    final static String DIGIT1 = "1";
+    final static String DIGIT2 = "2";
+    final static String DIGIT3 = "3";
+    final static String DIGIT4 = "4";
+    final static String DIGIT5 = "5";
+    final static String DIGIT6 = "6";
+    final static String DIGIT7 = "7";
+    final static String DIGIT8 = "8";
+    final static String DIGIT9 = "9";
+    final static String PLUSSIGN = "+";
+    final static String MINUSSIGN = "-";
+    final static String MULSIGN = "×";
+    final static String DIVSIGN = "÷";
+    final static String INVSIGN = "¹/ₓ";
+    final static String SQUARESIGN = "x²";
+    final static String ROOTSIGN = "√";
+    final static String CHANGESIGN = "±";
+    final static String EQUALSIGN = "=";
+    final static String ERASESIGN = "⌫";
+    final static String PERCSIGN = "%";
+
+    int number1, number2, number3;
+    String operation1, operation2;
+
+    public void add ( String sign ) {
+
+    }
+
+    public static void main ( String[] args ) {
+
+
+
+    }
+
+}
